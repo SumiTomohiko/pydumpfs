9744c7f8529034869980a577190e72bcc38260f1
SumiTomohiko <SumiTomohiko@neko-daisuki.ddo.jp>
2011-06-19 23:36:53 +0900

    Refactored to use only from statement for importing.

--- src/pydumpfs/tests/__init__.py
+++ src/pydumpfs/tests/__init__.py
@@ -1,35 +1,35 @@
 #! python
 # -*- coding: utf-8 -*-
 
-from os import makedirs
-from os.path import join
-import os
-import os.path
-import shutil
-import stat
-import unittest
+from os import chmod, lchown, listdir, lstat, makedirs, mkfifo, readlink, remove, stat, walk
+from os.path import abspath, dirname, exists, isdir, isfile, islink, join, lexists, samefile
+from shutil import rmtree
+from stat import S_IRUSR, S_IRWXG, S_IRWXO, S_IRWXU, S_ISLNK, S_ISREG
+from unittest import TestCase, main
 
 from sys import path
 path.insert(0, "src")
 
 from pydumpfs import Pydumpfs, PydumpfsError
 
-class TestPydumpfs(unittest.TestCase):
+class TestPydumpfs(TestCase):
 
     def setUp(self):
         import tempfile
         self.dest_dir = tempfile.mkdtemp(prefix="pydumpfs")
 
     def tearDown(self):
-        shutil.rmtree(self.dest_dir)
+        rmtree(self.dest_dir)
 
     def test_copy_dir(self):
         self._do_test("copy_dir")
 
+    def _do_test_twice(self, name):
+        for _ in range(2):
+            self._do_test(name)
+
     def test_copy_dir_twice(self):
-        dirname = "copy_dir_twice"
-        self._do_test(dirname)
-        self._do_test(dirname)
+        self._do_test_twice("copy_dir_twice")
 
     def test_copy_file(self):
         self._do_test("copy_file")
@@ -41,54 +41,54 @@ class TestPydumpfs(unittest.TestCase):
         self._do_test("copy_symlink_file")
 
     def test_hard_link(self):
-        dirname = "hard_link"
-        src_dir = self._get_source_directory(dirname)
+        name = "hard_link"
+        src_dir = self._get_source_directory(name)
 
         obj = Pydumpfs(**self._get_pydumpfs_options())
         backup_dir1 = obj.do(self.dest_dir, src_dir)
         backup_dir2 = obj.do(self.dest_dir, src_dir)
 
-        foo_src_path = os.path.join(src_dir, "foo")
+        foo_src_path = join(src_dir, "foo")
         path1 = backup_dir1 + foo_src_path
         path2 = backup_dir2 + foo_src_path
-        self.assert_(os.path.samefile(path1, path2), 
-            "%(path1)r's i-node and %(path2)r's one are not same." 
+        self.assert_(samefile(path1, path2),
+            "%(path1)r's i-node and %(path2)r's one are not same."
                 % dict(path1=path1, path2=path2))
 
     def test_new_file(self):
-        dirname = "new_file"
-        src_dir = self._get_source_directory(dirname)
-        file_path = os.path.join(src_dir, "foo")
-        if os.path.isfile(file_path):
-            os.remove(file_path)
-        elif os.path.isdir(file_path):
-            shutil.rmtree(file_path)
+        name = "new_file"
+        src_dir = self._get_source_directory(name)
+        file_path = join(src_dir, "foo")
+        if isfile(file_path):
+            remove(file_path)
+        elif isdir(file_path):
+            rmtree(file_path)
 
         obj = Pydumpfs()
         obj.do(self.dest_dir, src_dir)
 
         self._make_sample_file(file_path)
 
-        self._do_test(dirname)
+        self._do_test(name)
 
     def test_remove_file(self):
-        dirname = "remove_file"
-        src_dir = self._get_source_directory(dirname)
-        file_path = os.path.join(src_dir, "foo")
+        name = "remove_file"
+        src_dir = self._get_source_directory(name)
+        file_path = join(src_dir, "foo")
 
         self._make_sample_file(file_path)
 
         obj = Pydumpfs()
         obj.do(self.dest_dir, src_dir)
 
-        os.remove(file_path)
+        remove(file_path)
 
-        self._do_test(dirname)
+        self._do_test(name)
 
     def test_update_file(self):
-        dirname = "update_file"
-        src_dir = self._get_source_directory(dirname)
-        file_path = os.path.join(src_dir, "foo")
+        name = "update_file"
+        src_dir = self._get_source_directory(name)
+        file_path = join(src_dir, "foo")
 
         self._make_sample_file(file_path)
 
@@ -101,12 +101,12 @@ class TestPydumpfs(unittest.TestCase):
         finally:
             file.close()
 
-        self._do_test(dirname)
+        self._do_test(name)
 
     def test_change_uid(self):
-        dirname = "change_uid"
-        src_dir = self._get_source_directory(dirname)
-        file_path = os.path.join(src_dir, "foo")
+        name = "change_uid"
+        src_dir = self._get_source_directory(name)
+        file_path = join(src_dir, "foo")
 
         self._change_uid(file_path, 1000)
 
@@ -119,12 +119,12 @@ class TestPydumpfs(unittest.TestCase):
 
         path1 = backup_dir1 + file_path
         path2 = backup_dir2 + file_path
-        self.failIf(os.path.samefile(path1, path2))
+        self.failIf(samefile(path1, path2))
 
     def test_change_gid(self):
-        dirname = "change_gid"
-        src_dir = self._get_source_directory(dirname)
-        file_path = os.path.join(src_dir, "foo")
+        name = "change_gid"
+        src_dir = self._get_source_directory(name)
+        file_path = join(src_dir, "foo")
 
         self._change_gid(file_path, 1000)
 
@@ -137,87 +137,83 @@ class TestPydumpfs(unittest.TestCase):
 
         path1 = backup_dir1 + file_path
         path2 = backup_dir2 + file_path
-        self.failIf(os.path.samefile(path1, path2))
+        self.failIf(samefile(path1, path2))
 
     def test_mode(self):
-        dirname = "mode"
-        src_dir = self._get_source_directory(dirname)
-        file_path = os.path.join(src_dir, "foo")
+        name = "mode"
+        src_dir = self._get_source_directory(name)
+        file_path = join(src_dir, "foo")
 
-        os.chmod(file_path, stat.S_IRWXU | stat.S_IRWXG | stat.S_IRWXO)
+        chmod(file_path, S_IRWXU | S_IRWXG | S_IRWXO)
 
-        self._do_test(dirname)
+        self._do_test(name)
 
     def test_change_mode(self):
-        dirname = "change_mode"
-        src_dir = self._get_source_directory(dirname)
-        file_path = os.path.join(src_dir, "foo")
+        name = "change_mode"
+        src_dir = self._get_source_directory(name)
+        file_path = join(src_dir, "foo")
 
-        os.chmod(file_path, stat.S_IRWXU | stat.S_IRWXG | stat.S_IRWXO)
+        chmod(file_path, S_IRWXU | S_IRWXG | S_IRWXO)
 
         obj = Pydumpfs(**self._get_pydumpfs_options())
         backup_dir1 = obj.do(self.dest_dir, src_dir)
 
-        os.chmod(file_path, stat.S_IRUSR)
+        chmod(file_path, S_IRUSR)
 
         backup_dir2 = obj.do(self.dest_dir, src_dir)
 
         path1 = backup_dir1 + file_path
         path2 = backup_dir2 + file_path
-        self.failIf(os.path.samefile(path1, path2))
+        self.failIf(samefile(path1, path2))
 
     def test_copy_symlink_dir_twice(self):
-        dirname = "copy_symlink_dir_twice"
-        self._do_test(dirname)
-        self._do_test(dirname)
+        self._do_test_twice("copy_symlink_dir_twice")
 
     def test_copy_symlink_file_twice(self):
-        dirname = "copy_symlink_file_twice"
-        self._do_test(dirname)
-        self._do_test(dirname)
+        self._do_test_twice("copy_symlink_file_twice")
 
     def test_different_uid_symlink_file(self):
-        dirname = "different_uid_symlink_file"
-        src_dir = self._get_source_directory(dirname)
-        foo_path = os.path.join(src_dir, "foo")
-        bar_path = os.path.join(src_dir, "bar")
+        name = "different_uid_symlink_file"
+        src_dir = self._get_source_directory(name)
+        foo_path = join(src_dir, "foo")
+        bar_path = join(src_dir, "bar")
 
         self._change_uid(foo_path, 1000)
         self._change_uid(bar_path, 0)
 
-        self._do_test(dirname)
+        self._do_test(name)
 
     def test_different_gid_symlink_file(self):
-        dirname = "different_gid_symlink_file"
-        src_dir = self._get_source_directory(dirname)
-        foo_path = os.path.join(src_dir, "foo")
-        bar_path = os.path.join(src_dir, "bar")
+        name = "different_gid_symlink_file"
+        src_dir = self._get_source_directory(name)
+        foo_path = join(src_dir, "foo")
+        bar_path = join(src_dir, "bar")
 
         self._change_gid(foo_path, 1000)
         self._change_gid(bar_path, 0)
 
-        self._do_test(dirname)
+        self._do_test(name)
 
     def test_copy_dead_link(self):
-        dirname = "copy_dead_link"
-        src_dir = self._get_source_directory(dirname)
-        foo_path = os.path.join(src_dir, "foo")
-        os.remove(foo_path)
+        name = "copy_dead_link"
+        src_dir = self._get_source_directory(name)
+        foo_path = join(src_dir, "foo")
+        remove(foo_path)
         try:
             self._do_test("copy_dead_link")
         finally:
             self._make_sample_file(foo_path)
 
     def test_new_file_after_copy(self):
-        dirname = "new_file_after_copy"
-        src_dir = self._get_source_directory(dirname)
+        name = "new_file_after_copy"
+        src_dir = self._get_source_directory(name)
 
-        foo_path = os.path.join(src_dir, "foo")
-        if os.path.exists(foo_path):
-            os.remove(foo_path)
+        foo_path = join(src_dir, "foo")
+        if exists(foo_path):
+            remove(foo_path)
 
         obj = Pydumpfs(**self._get_pydumpfs_options())
-        os.makedirs(self.dest_dir + src_dir)
+        makedirs(self.dest_dir + src_dir)
         obj._copy_recursively(self.dest_dir, src_dir)
 
         self._make_sample_file(foo_path)
@@ -225,46 +221,45 @@ class TestPydumpfs(unittest.TestCase):
         obj._change_meta_data(self.dest_dir, src_dir)
 
     def test_fifo(self):
-        dirname = "fifo"
-        src_dir = self._get_source_directory(dirname)
-        foo_path = os.path.join(src_dir, "foo")
-        os.mkfifo(foo_path)
+        name = "fifo"
+        src_dir = self._get_source_directory(name)
+        foo_path = join(src_dir, "foo")
+        mkfifo(foo_path)
         try:
-            self._do_test(dirname)
+            self._do_test(name)
         finally:
-            os.remove(foo_path)
+            remove(foo_path)
 
     def test_fifo_twice(self):
-        dirname = "fifo_twice"
-        src_dir = self._get_source_directory(dirname)
-        foo_path = os.path.join(src_dir, "foo")
-        os.mkfifo(foo_path)
+        name = "fifo_twice"
+        src_dir = self._get_source_directory(name)
+        foo_path = join(src_dir, "foo")
+        mkfifo(foo_path)
         try:
-            self._do_test(dirname)
-            self._do_test(dirname)
+            self._do_test_twice(name)
         finally:
-            os.remove(foo_path)
+            remove(foo_path)
 
     def test_same_name_dir(self):
-        dirname = "same_name_dir"
-        src_dir = self._get_source_directory(dirname)
+        name = "same_name_dir"
+        src_dir = self._get_source_directory(name)
         filename = "foo"
         prev_dir = join(self.dest_dir, "2008", "01", "06", "000000.000000")
         illegal_dir = join(prev_dir + src_dir, filename)
         makedirs(illegal_dir)
 
-        self._do_test(dirname)
+        self._do_test(name)
 
     def _change_uid(self, path, uid):
-        st = os.stat(path)
-        os.lchown(path, uid, st.st_gid)
+        st = stat(path)
+        lchown(path, uid, st.st_gid)
 
     def _change_gid(self, path, gid):
-        st = os.stat(path)
-        os.lchown(path, st.st_uid, gid)
+        st = stat(path)
+        lchown(path, st.st_uid, gid)
 
-    def _get_source_directory(self, dirname):
-        return os.path.abspath(os.path.join(os.path.dirname(__file__), dirname))
+    def _get_source_directory(self, name):
+        return abspath(join(dirname(__file__), name))
 
     def _make_sample_file(self, path):
         file = open(path, "w")
@@ -275,93 +270,91 @@ class TestPydumpfs(unittest.TestCase):
 
     def _compare_file(self, path1, path2):
         import filecmp
-        self.assert_(filecmp.cmp(path1, path2), 
-            "%(path1)r and %(path2)r are not same." 
+        self.assert_(filecmp.cmp(path1, path2),
+            "%(path1)r and %(path2)r are not same."
                 % dict(path1=path1, path2=path2))
 
     def _compare_stat_attribute(self, path1, path2, name):
-        st1 = os.lstat(path1)
-        st2 = os.lstat(path2)
+        st1 = lstat(path1)
+        st2 = lstat(path2)
         value1 = st1.__getattribute__(name)
         value2 = st2.__getattribute__(name)
 
-        self.assert_(value1 == value2, 
-            "%(name)r of %(path1)r and that of %(path2)r are not same." 
+        self.assert_(value1 == value2,
+            "%(name)r of %(path1)r and that of %(path2)r are not same."
                 % dict(path1=path1, path2=path2, name=name))
 
     def _compare_stat(self, path1, path2):
         self._compare_stat_attribute(path1, path2, "st_uid")
         self._compare_stat_attribute(path1, path2, "st_gid")
-        if (not os.path.islink(path1)) and (not os.path.islink(path2)):
+        if (not islink(path1)) and (not islink(path2)):
             self._compare_stat_attribute(path1, path2, "st_mode")
             self._compare_stat_attribute(path1, path2, "st_mtime")
 
     def _compare_link(self, path1, path2):
-        if os.path.islink(path1):
-            self.assert_(os.path.islink(path2), 
-                "%(path1)r is a symbolic link, but %(path2)r is not." 
+        if islink(path1):
+            self.assert_(islink(path2),
+                "%(path1)r is a symbolic link, but %(path2)r is not."
                     % dict(path1=path1, path2=path2))
 
-            link1 = os.readlink(path1)
-            link2 = os.readlink(path2)
-            self.assert_(link1 == link2, 
+            link1 = readlink(path1)
+            link2 = readlink(path2)
+            self.assert_(link1 == link2,
                 "The symbolic link %(path1)r and %(path2)r doesn't link to a sa"
                 "me file/directory." % dict(path1=path1, path2=path2))
         else:
-            self.assert_(not os.path.islink(path2), 
-                "%(path1)r is not a symbolic link, but %(path2)r is." 
+            self.assert_(not islink(path2),
+                "%(path1)r is not a symbolic link, but %(path2)r is."
                     % dict(path1=path1, path2=path2))
 
     def _compare_file_node(self, backup_dir, dirpath, name, is_func):
-        src_path = os.path.join(dirpath, name)
+        src_path = join(dirpath, name)
         backup_path = backup_dir + src_path
-        if not os.path.islink(backup_path):
-            self.assert_(is_func(backup_path), 
+        if not islink(backup_path):
+            self.assert_(is_func(backup_path),
                 "%(path)r is not a file/directory." % dict(path=backup_path))
 
         self._compare_link(backup_path, src_path)
         self._compare_stat(backup_path, src_path)
 
     def _compare_dir_recursively(self, backup_dir, src_dir):
-        src_dir = os.path.abspath(src_dir)
+        src_dir = abspath(src_dir)
 
-        dir = os.path.dirname(src_dir)
+        dir = dirname(src_dir)
         while dir != "/":
-            self.assert_(os.path.isdir(dir), 
+            self.assert_(isdir(dir),
                 "%(dir)r is not a directory." % dict(dir=dir))
             self._compare_stat(backup_dir + dir, dir)
+            dir = dirname(dir)
 
-            dir = os.path.dirname(dir)
+        for dirpath, dirnames, filenames in walk(src_dir):
+            dirpath = abspath(dirpath)
 
-        for dirpath, dirnames, filenames in os.walk(src_dir):
-            dirpath = os.path.abspath(dirpath)
-
-            for dirname in dirnames:
-                self._compare_file_node(
-                    backup_dir, dirpath, dirname, os.path.isdir)
+            for name in dirnames:
+                self._compare_file_node(backup_dir, dirpath, name, isdir)
 
             for filename in filenames:
-                path = os.path.join(dirpath, filename)
+                path = join(dirpath, filename)
                 backup_path = backup_dir + path
-                st = os.lstat(path)
-                if stat.S_ISREG(st.st_mode) or stat.S_ISLNK(st.st_mode):
+                st = lstat(path)
+                if S_ISREG(st.st_mode) or S_ISLNK(st.st_mode):
                     self._compare_file_node(
-                        backup_dir, dirpath, filename, os.path.isfile)
-                    path = os.path.join(dirpath, filename)
-                    if not os.path.islink(path):
+                        backup_dir, dirpath, filename, isfile)
+                    path = join(dirpath, filename)
+                    if not islink(path):
                         self._compare_file(backup_path, path)
                 else:
-                    self.failIf(os.path.lexists(backup_path), 
+                    self.failIf(lexists(backup_path),
                         "%(src)r isn't a file nor a link. But %(dest)r exists."
                             % dict(src=path, dest=backup_path))
 
-            for name in os.listdir(backup_dir + dirpath):
-                self.assert_((name in dirnames) or (name in filenames), 
-                    "%(path)r is not in a source." 
-                        % dict(path=backup_dir + os.path.join(dirpath, name)))
+            for name in listdir(backup_dir + dirpath):
+                self.assert_((name in dirnames) or (name in filenames),
+                    "%(path)r is not in a source."
+                        % dict(path=backup_dir + join(dirpath, name)))
 
-    def _do_test(self, dirname):
-        src_dir = self._get_source_directory(dirname)
+    def _do_test(self, name):
+        src_dir = self._get_source_directory(name)
 
         obj = Pydumpfs(**self._get_pydumpfs_options())
         backup_dir = obj.do(self.dest_dir, src_dir)
@@ -384,6 +377,6 @@ class TestPydumpfs(unittest.TestCase):
         return {}
 
 if __name__ == "__main__":
-    unittest.main()
+    main()
 
 # vim: tabstop=4 shiftwidth=4 expandtab softtabstop=4
